38f60f38773d81c4f253289bf33fd4d2349a8b7d
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-01 16:42:42 +0100

    Update ExternalKeyboard.java

--- src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
+++ src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
@@ -17,53 +17,13 @@ import org.json.JSONObject;
 	https://stackoverflow.com/questions/40375555/start-android-activity-from-cordova-plugin
  */
 public class ExternalKeyboard extends CordovaPlugin {
-
-	private static final String ACTION = "com.symbol.ekb.api.ACTION_UPDATE";
-	private static final String PACKAGE = "com.symbol.mxmf.csp.enterprisekeyboard";
-
-    /**
-     * Executes the request and returns PluginResult.
-     *
-     * @param action        The action to execute.
-     * @param args          JSONArry of arguments for the plugin.
-     * @param callbackId    The callback id used when calling back into JavaScript.
-     * @return              A PluginResult object with a status and message.
-     */
-            
+        
 	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-		Context context = cordova.getActivity().getApplicationContext();
-		if ("selectLayout".equals(action)) {
-			String layout;
-			String layoutGroupName;
-		  try {
-			JSONObject options = args.getJSONObject(0);
-			layout = options.getString("layout");
-			layoutGroupName = options.getString("layoutGroupName");
-		  } catch (JSONException e) {
-			callbackContext.error("Error encountered: " + e.getMessage());
-			return false;
-		  }
-			this.selectLayout(layoutGroupName,layout,callbackContext,context);  
-			  // Send a positive result to the callbackContext		
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {	
 			return true;
-		}
-		return false;  // Returning false results in a "MethodNotFound" error.
 	}
 	
-	private void selectLayout(String layoutGroupName, String layout,CallbackContext callbackContext,Context context) {
-		if (layoutGroupName != null && layoutGroupName.length() > 0 && layout != null && layout.length() > 0) {
-				Intent intent = new Intent();
-				intent.setAction(ACTION);
-				intent.setPackage(PACKAGE);
-				intent.addFlags(Intent.FLAG_RECEIVER_FOREGROUND);
-				intent.putExtra("CURRENT_LAYOUT_GROUP", layoutGroupName);
-				intent.putExtra("CURRENT_LAYOUT_NAME", layout);
-				//this.cordova.getActivity().startActivity(intent);
-				context.sendBroadcast(intent);  
+	private void selectLayout(String layoutGroupName, String layout,CallbackContext callbackContext,Context context) { 
 				callbackContext.success();
-		} else {
-			callbackContext.error("Expected one non-empty string argument.");
-		}
 	}
 }
