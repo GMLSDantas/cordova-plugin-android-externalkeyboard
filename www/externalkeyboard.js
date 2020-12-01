0bc6c2fa4f75b72f7e9135145e2837f19639dcf5
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-01 16:03:24 +0100

    Update and rename zebrakeyboard.js to externalkeyboard.js

--- www/externalkeyboard.js
+++ www/externalkeyboard.js
@@ -1,21 +1,21 @@
 // Empty constructor
-function zebraKeyboard() {}
+function externalKeyboard() {}
 
 // The function that passes work along to native shells
 // layoutGroupName is a string (default: "Vanboxtel_Null_Keyboard", layout is a string (default: "Null_Keyboard")
-zebraKeyboard.prototype.selectLayout = function(successCallback, errorCallback,layoutGroupName,layout) {
+externalKeyboard.prototype.selectLayout = function(successCallback, errorCallback,layoutGroupName,layout) {
   var options = {};
   options.layoutGroupName = String(layoutGroupName);
   options.layout = String(layout);
-  cordova.exec(successCallback, errorCallback, 'ZebraKeyboard', 'selectLayout', [options]);
+  cordova.exec(successCallback, errorCallback, 'ExternalKeyboard', 'selectLayout', [options]);
 }
 
 // Installation constructor that binds ZebraKeyboardPlugin to window
-zebraKeyboard.install = function() {
+externalKeyboard.install = function() {
   if (!window.plugins) {
     window.plugins = {};
   }
-  window.plugins.zebraKeyboard = new zebraKeyboard();
-  return window.plugins.zebraKeyboard;
+  window.plugins.externalKeyboard = new externalKeyboard();
+  return window.plugins.externalKeyboard;
 };
-cordova.addConstructor(zebraKeyboard.install);
+cordova.addConstructor(externalKeyboard.install);
