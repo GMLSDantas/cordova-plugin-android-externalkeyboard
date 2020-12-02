3166adea4c93bb312a6e25139a4567e42e44740e
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-02 11:02:14 +0100

    Update ExternalKeyboard.java

--- src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
+++ src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
@@ -2,7 +2,7 @@ package com.GMLSDantas.plugin;
 
 import android.content.Intent;
 import android.content.Context;
-import android.content.res.*;
+import android.content.res.Configuration;
 // Cordova-required packages
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
@@ -12,21 +12,13 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-/**
- * This class calls a specific template from the zebra keyboard.
- https://developer.android.com/reference/android/content/Intent
- https://www.javatpoint.com/android-intent-tutorial
- https://techdocs.zebra.com/enterprise-keyboard/latest/guide/apis/
- https://stackoverflow.com/questions/40375555/start-android-activity-from-cordova-plugin
- 38f60f3  - works
- ad2dee4  - works
- */
 public class ExternalKeyboard extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-
-        int keyboard = getResources().getConfiguration().keyboard;
+        Context context = cordova.getActivity().getApplicationContext();
+        int keyboard;
+        keyboard = getResources().getConfiguration().keyboard;
         if (keyboard != Configuration.KEYBOARD_NOKEYS) {
             Log.d("WT6000_keypadapp","HARDWARE KEYBOARD: CONNECTED");
         } else {
