72f61c3bb2a715113c7b9cb14f26e736fc570c9e
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-02 13:05:53 +0100

    Update ExternalKeyboard.java

--- src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
+++ src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
@@ -16,8 +16,7 @@ public class ExternalKeyboard extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        Configuration configuration = cordova.getActivity().getResouces().getConfiguration();
-        //int keyboard = configuration.keyboard;
-        return true;
+     
+            Configuration config = getResources().getConfiguration();
     }
 }
