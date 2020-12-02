00d9861f1b78e10588e1e0b95f157f7ccd9a2b16
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-02 10:44:14 +0100

    Update ExternalKeyboard.java

--- src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
+++ src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
@@ -7,31 +7,32 @@ import android.content.res.*;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import android.content.res.Resources;
+import android.util.Log;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
  * This class calls a specific template from the zebra keyboard.
-	https://developer.android.com/reference/android/content/Intent
-	https://www.javatpoint.com/android-intent-tutorial
-	https://techdocs.zebra.com/enterprise-keyboard/latest/guide/apis/
-	https://stackoverflow.com/questions/40375555/start-android-activity-from-cordova-plugin
-	38f60f3  - works
-	ad2dee4  - works
+ https://developer.android.com/reference/android/content/Intent
+ https://www.javatpoint.com/android-intent-tutorial
+ https://techdocs.zebra.com/enterprise-keyboard/latest/guide/apis/
+ https://stackoverflow.com/questions/40375555/start-android-activity-from-cordova-plugin
+ 38f60f3  - works
+ ad2dee4  - works
  */
 public class ExternalKeyboard extends CordovaPlugin {
-        
-	@Override
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 
-		int keyboard = getResources().getConfiguration().keyboard;
-		if (keyboard != Configuration.KEYBOARD_NOKEYS) {
-        		Log.d("WT6000_keypadapp","HARDWARE KEYBOARD: CONNECTED");
-          	} else {
-        		Log.d("WT6000_keypadapp","HARDWARE KEYBOARD: NOT CONNECTED"); 
-        	}
-		return true;
-	
-	}
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+
+        int keyboard = getResources().getConfiguration().keyboard;
+        if (keyboard != Configuration.KEYBOARD_NOKEYS) {
+            Log.d("WT6000_keypadapp","HARDWARE KEYBOARD: CONNECTED");
+        } else {
+            Log.d("WT6000_keypadapp","HARDWARE KEYBOARD: NOT CONNECTED");
+        }
+        return true;
+
+    }
 }
