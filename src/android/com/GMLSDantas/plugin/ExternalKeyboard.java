f6dfa42ef5774f7a10962c32fb6a424b89ed0193
GMLSDantas <75125502+GMLSDantas@users.noreply.github.com>
2020-12-03 14:41:08 +0100

    Update ExternalKeyboard.java

--- src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
+++ src/android/com/GMLSDantas/plugin/ExternalKeyboard.java
@@ -30,9 +30,9 @@ public class ExternalKeyboard extends CordovaPlugin {
         Configuration config = context.getResources().getConfiguration();
         int keyboard = config.keyboard;
         if (keyboard != Configuration.KEYBOARD_NOKEYS) {
-            callbackContext.success('true');
+            callbackContext.success("true");
         } else {
-            callbackContext.success('false');
+            callbackContext.success("false");
         }
 
     }
